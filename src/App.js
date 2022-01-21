2ebee09562f111ac97b18fd9e8e0233d81b097c9
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-01-21 08:40:22 -0800

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled, { keyframes } from 'styled-components';
 import './App.css';
+import Character from './components/Character';
+
+const kf = keyframes`
+  50% {
+    transform: scale(2.3);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }`
+
+  const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 3px dotted #0DA85A;
+
+  h2 {
+    color: orange;
+    border: 2px red;
+  }
+  h3 {
+    color: #0DA85A;
+  }
+
+  transform: scale(2); // start of animation
+  opacity: 0; // start of animation;
+  animation: ${kf} 3s ease-in-out forwards;
+`
 
 const App = () => {
+  const [charData, setCharData] = useState([]);
+ 
+  useEffect(() => {
+    const fetchData = ()  => {
+      axios.get('https://swapi.dev/api/people')
+      .then((res) => {
+        console.log(res.data)
+        setCharData(res.data);  
+      }).catch(err => {
+        console.error(err);
+      })
+    }
+    fetchData();
+  }, [])
+console.log(charData[0]);
+
+
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -10,9 +58,17 @@ const App = () => {
   // sync up with, if any.
 
   return (
-    <div className="App">
+    <StyledApp className="App">
       <h1 className="Header">Characters</h1>
-    </div>
+      {
+        charData.map(char => {
+          return <Character info={char} key={char.name}/>
+        })
+      }
+     
+      
+
+    </StyledApp>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import react from "react"
+
+
+
+
+const Character = props => {
+    const { info } = props
+    return (
+        <div>
+            <h2>{info.name}</h2>
+            <p>Gender: {info.gender}</p>
+            <p>birth Year: {info.birth_year} </p>
+            <h3>Hair: {info.hair_color} Eyes: {info.eye_color}</h3> 
+        </div>
+    )
+    
+}
+
+export default Character;
